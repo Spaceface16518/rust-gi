faf4f782ce012e2bd4954e78986c53b30d7d5845
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-15 00:57:19 -0500

    Add `check` method
    Checks to see if any caches exist. If they do, then it returns all of them as`DirEntry`s

--- src/cache.rs
+++ src/cache.rs
@@ -1,6 +1,7 @@
-use std::fs::File;
+use std::fs::{File, DirEntry};
 use std::io::Error;
 use std::path::Path;
+use std::ffi::OsString;
 
 pub const CACHE_NAME: &str = ".gi_cache";
 
@@ -9,5 +10,24 @@ pub fn init(path: &Path) -> Result<File, Error> {
         init(path.parent().unwrap()) // Take parent instead of excepting?
     } else {
         Ok(File::create(path.join(Path::new(CACHE_NAME)).as_path()).unwrap())
+    }
+}
+
+pub fn check(path: &Path) -> Option<Vec<DirEntry>> {
+    if !path.is_dir() {
+        check(path.parent().unwrap())
+    } else {
+        let mut found: Vec<DirEntry> = Vec::new();
+        for entry in path.read_dir().unwrap() {
+            let entry = entry.unwrap();
+            if entry.file_name() == OsString::from(CACHE_NAME) {
+                found.push(entry);
+            }
+        }
+        if found.is_empty() {
+            None
+        } else {
+            Some(found)
+        }
     }
 }
