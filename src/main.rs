29b5cf10736323b04636d98c1d70742650c05416
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-14 19:46:39 -0500

    Add proper imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate curl;
+
+use std::env::args;
+use std::io::stdout;
+use std::io::Write;
+
+use curl::easy::Easy;
+
+const PREFIX: &str = "https://www.gitignore.io/api/";
+
 fn main() {
     println!("Hello, world!");
 }
