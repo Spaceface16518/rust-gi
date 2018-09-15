8cd8be161f2ec5a7062374414b8c7a750e65a64f
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-14 23:18:42 -0500

    Abstract argv processing and formatting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 extern crate curl;
 
-use std::env::args;
-use std::io::{stdout, Write};
 use curl::easy::Easy;
+use std::env::{args, Args};
+use std::io::Error;
+use std::io::{stdout, Write};
 
 const PREFIX: &str = "https://www.gitignore.io/api/";
 
 fn main() {
-    let mut args: Vec<String> = args().collect();
-    args.remove(0);
-    let a_str: String = format!("{}{}", PREFIX, args.join(","));
+    let argv: Vec<String> = proc_args(args());
+    let a_str: String = format(PREFIX, argv);
     let mut easy = Easy::new();
     easy.url(a_str.as_str()).unwrap();
-    easy.write_function(|data| Ok(stdout().write(data).unwrap()))
+    easy.write_function(|data: &[u8]| Ok(stdout().write(data).unwrap()))
         .unwrap();
     easy.perform().unwrap();
+}
+
+fn proc_args(argv: Args) -> Vec<String> {
+    let mut a: Vec<String> = argv.collect();
+    a.remove(0);
+    return a;
+}
+
+fn format(prefix: &str, args: Vec<String>) -> String {
+    format!("{}{}", prefix, args.join(","))
 }
