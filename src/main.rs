74e1d9e0660be84252f0a9041aec7b5368884fe8
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-14 19:47:23 -0500

    Add curl operation

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,9 @@ fn main() {
     let mut args: Vec<String> = args().collect();
     args.remove(0);
     let a_str: String = format!("{}{}", PREFIX, args.join(","));
+    let mut easy = Easy::new();
+    easy.url(a_str.as_str()).unwrap();
+    easy.write_function(|data| Ok(stdout().write(data).unwrap()))
+        .unwrap();
+    easy.perform().unwrap();
 }
