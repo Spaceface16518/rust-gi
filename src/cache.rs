ac7a8923b75d5361ef697c26b92f21e7adbb24eb
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-09-15 01:03:50 -0500

    Abstract internals
    This is to abstract the Result and Option checking and have the internal, actual methods run in separate methods.

--- src/cache.rs
+++ src/cache.rs
@@ -7,27 +7,37 @@ pub const CACHE_NAME: &str = ".gi_cache";
 
 pub fn init(path: &Path) -> Result<File, Error> {
     if !path.is_dir() {
-        init(path.parent().unwrap()) // Take parent instead of excepting?
+        Ok(i_init(path.parent().unwrap())) // Take parent instead of excepting?
     } else {
-        Ok(File::create(path.join(Path::new(CACHE_NAME)).as_path()).unwrap())
+        Ok(i_init(path))
     }
+}
+
+fn i_init(path: &Path) -> File {
+    File::create(path.join(Path::new(CACHE_NAME)).as_path()).unwrap()
 }
 
 pub fn check(path: &Path) -> Option<Vec<DirEntry>> {
     if !path.is_dir() {
-        check(path.parent().unwrap())
+        Some(i_check(path.parent().unwrap()))
     } else {
-        let mut found: Vec<DirEntry> = Vec::new();
+        let f = i_check(path);
+        if f.is_empty() {
+            None
+        } else {
+            Some(f)
+        }
+    }
+}
+
+fn i_check(path: &Path) -> Vec<DirEntry> {
+let mut found: Vec<DirEntry> = Vec::new();
         for entry in path.read_dir().unwrap() {
             let entry = entry.unwrap();
             if entry.file_name() == OsString::from(CACHE_NAME) {
                 found.push(entry);
             }
         }
-        if found.is_empty() {
-            None
-        } else {
-            Some(found)
-        }
-    }
+return found;
 }
+
