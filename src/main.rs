70ba7fed4f0ad279658094851b60726da279ad2d
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-01-11 10:40:39 -0600

    Migrate to hyper.rs

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,39 @@
-#[macro_use]
-extern crate log;
-extern crate curl;
-extern crate pretty_env_logger;
-
-use curl::easy::{Easy, WriteError};
-use out::{write, Output};
-use std::{
-    fs::{File, OpenOptions},
-    io::Write,
+use hyper::{
+    rt::{self, Future, Stream},
+    Client,
 };
+use hyper_tls::HttpsConnector;
+use std::{
+    env::args,
+    io::{self, Write},
+};
+use log::{error, info};
 
-mod app;
-mod out;
-
-const PREFIX: &str = "https://www.gitignore.io/api/";
+mod uri;
 
 fn main() {
     pretty_env_logger::init();
-    let (input, mut out) = proc_args();
-    let a_str: String = format(PREFIX, input);
-    let mut easy = Easy::new();
-    easy.url(a_str.as_str()).unwrap();
-    easy.write_function(move |data| write_fn(data, &mut out)).unwrap();
-    easy.perform().unwrap();
+    let args = args().skip(1).collect::<Vec<String>>();
+    let uri = uri::uri_from(args);
+
+    rt::run(rt::lazy(|| {
+        let client = Client::builder().build::<_, hyper::Body>(
+            HttpsConnector::new(4).expect("TLS initialization failed"),
+        );
+        info!("Client connected");
+        client
+            .get(uri)
+            .and_then(|res| {
+                res.into_body()
+                    .for_each(|chunk| {
+                        io::stdout().write_all(&chunk).map_err(|e| {
+                            panic!("Error writing to stdout, error={}", e)
+                        })
+                    })
+            })
+            .map_err(|err| {
+                error!("{}", err);
+            })
+    }));
     info!("Success");
-}
-
-fn proc_args() -> (Vec<String>, Output<File>) {
-    trace!("Processing args...");
-    let matches = app::app().get_matches();
-
-    (
-        matches
-            .value_of("INPUT")
-            .unwrap()
-            .split(" ")
-            .map(String::from)
-            .collect::<Vec<String>>(),
-        match matches.value_of("OUTPUT") {
-            Some(name) => {
-                match OpenOptions::new().create(true).write(true).open(name) {
-                    Ok(handle) => Output::Other(handle),
-                    Err(_) => {
-                        error!("There was an error trying to use that location as an output; using standard output instead");
-                        Output::Stdout
-                    },
-                }
-            },
-            None => Output::Stdout,
-        },
-    )
-}
-
-fn format(prefix: &str, args: Vec<String>) -> String {
-    trace!("Formatting args...");
-    format!("{}{}", prefix, args.join(","))
-}
-
-fn write_fn<O: Write>(
-    data: &[u8],
-    out: &mut Output<O>,
-) -> Result<usize, WriteError> {
-    trace!("Writing data");
-    match write(out, data) {
-        Ok(i) => Ok(i),
-        Err(e) => panic!("Error in write function {:?}", e),
-    }
 }
---
+++ src/uri.rs
@@ -0,0 +1,57 @@
+use hyper::{http::HttpTryFrom, Uri};
+use log::trace;
+
+const PREFIX: &str = "https://www.gitignore.io/api/";
+
+pub fn uri_from(args: Vec<String>) -> Uri {
+    trace!("Creating URI from provided arguments");
+    Uri::try_from(format_uri(format_args(args)))
+        .expect("Could not generate URI")
+}
+
+fn format_args(args: Vec<String>) -> String { args.join(",") }
+
+fn format_uri(formatted: String) -> String {
+    format!("{}{}", PREFIX, formatted)
+}
+
+#[cfg(test)]
+mod uri_tests {
+    use super::*;
+
+    use std::str::FromStr;
+
+    #[test]
+    fn test_format_args() {
+        let args = vec!["arg1", "arg2", "arg3"]
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<String>>();
+        let formatted = format_args(args);
+        assert_eq!(formatted, "arg1,arg2,arg3".to_string());
+    }
+
+    #[test]
+    fn test_uri_from() {
+        let uri = uri_from(
+            vec!["arg1", "arg2", "arg3"]
+                .into_iter()
+                .map(String::from)
+                .collect::<Vec<String>>(),
+        );
+        assert_eq!(
+            uri,
+            Uri::from_str("https://www.gitignore.io/api/arg1,arg2,arg3")
+                .unwrap()
+        );
+    }
+
+    #[test]
+    fn test_format_uri() {
+        let formatted = format_uri("postfix".to_string());
+        assert_eq!(
+            formatted,
+            "https://www.gitignore.io/api/postfix".to_string()
+        );
+    }
+}
