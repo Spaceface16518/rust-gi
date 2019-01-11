b770ea7911544b7046a728e4be1f07d083047a2b
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-01-11 10:34:09 -0600

    Delete unnecessary files

--- src/app/statics.rs
+++
@@ -1,5 +0,0 @@
-pub const LONG_DESC: &str =
-    "This is a command line app to generate gitignores. You can use it easily right
- along with command line git with the `gi` command. It uses the public API to 
- generate gitignores, and therefore requires internet connection. Requests can 
- sometimes be cached for offline usage.";
